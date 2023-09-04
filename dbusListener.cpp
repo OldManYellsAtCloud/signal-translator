7b6f2f79540e9113e4d5703304ee519e8835f8d6
skandigraun <skandigraun@gmail.com>
2023-09-04 20:50:01 +0200

    change observed gesture direction, bottom-to-top caused space presses

--- dbusListener.cpp
+++ dbusListener.cpp
@@ -5,7 +5,7 @@
 void dbusListener::onTouchEvent(const std::string &direction)
 {
     LOG(LOG_DEBUG, "Direction arrived: " + direction);
-    if (direction == "BOTTOM_TO_TOP") {
+    if (direction == "LEFT_TO_RIGHT") {
         int res = kill(keyboard_pid_, KEYBOARD_TOGGLE_SIGNAL);
         if (res) {
             Logger::getLogger().log(LOG_ERR, "Could not send keyboard toggle signal: " + errno);
